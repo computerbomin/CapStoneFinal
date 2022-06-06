5c660e00dece8eada78b7e7150ab5e2b6845d359
chodonghee-hub <67031025+chodonghee-hub@users.noreply.github.com>
2022-06-06 21:50:26 +0900

    dh_commit - set widget status

--- lastCapStone/example/lib/login_app/login_refactor.dart
+++ lastCapStone/example/lib/login_app/login_refactor.dart
@@ -28,15 +28,15 @@ class _LogInRefacState extends State<LogInRefac> {
       child: Scaffold(
         backgroundColor: Colors.orangeAccent,
         appBar: AppBar(
-          title: Text(
-            "T4 캘린더",
-            style: TextStyle(
-              fontFamily: 'Gamja_Flower',
-              fontSize: 25.0,
-              fontWeight: FontWeight.bold,
-              color: Colors.white,
-            ),
-          ),
+          // title: Text(
+          //   "T4 캘린더",
+          //   style: TextStyle(
+          //     fontFamily: 'Gamja_Flower',
+          //     fontSize: 25.0,
+          //     fontWeight: FontWeight.bold,
+          //     color: Colors.white,
+          //   ),
+          // ),
           centerTitle: true,
           elevation: 0,
           backgroundColor: Colors.orangeAccent,
@@ -53,67 +53,40 @@ class _LogInRefacState extends State<LogInRefac> {
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
-          /*
-          MyButton(
-            image: Image.asset('image/glogo.png'),
-            text: Text(
-              'Login with Google',
-              style: TextStyle(color: Colors.black, fontSize: 15.0),
-            ),
-            color: Colors.white,
-            radius: 4.0,
-            onPressed: (){
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => GoogleLogin()),
-              );
-            },
-          ),
-
-          SizedBox(
-            height: 10.0,
-          ),
-
-          MyButton(
-            image: Image.asset('image/klogo.png'),
-            text: Text(
-              'Login with KakaoTalk',
-              style: TextStyle(color: Colors.brown, fontSize: 15.0),
-            ),
-            color: Colors.amberAccent,
-            radius: 4.0,
-            onPressed: () async {
-              await viewModel.login();
-              setState((){});
-            },
-          ),
-
-          SizedBox(
-            height: 10.0,
-          ),
-           */
-          CircleAvatar(
-            radius: 200,
-            backgroundColor: Colors.white,
-            child: Padding(
-              padding: const EdgeInsets.all(5), // Border radius
-              child: ClipOval(child: Image.asset('image/close.png'))),
-            ),
-          MyButton(
-            image: Icon(
-              Icons.mail,
-              color: Colors.white,
-            ),
-            text: Text(
-              '   로그인',
-              style: TextStyle(
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "GOOSE",
+                style: TextStyle(
+                  fontFamily: 'Gamja_Flower',
+                  fontSize: 70.0,
+                  fontWeight: FontWeight.bold,
                   color: Colors.white,
-                  fontSize: 20.0,
-                  fontFamily: 'Gamja_Flower'),
+                ),
+              ),
+            ]
+          ),
+          SizedBox(
+            height: 30,
+          ),
+
+          InkWell(
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                CircleAvatar(
+                  radius: 200,
+                  backgroundColor: Colors.white,
+                  child: Padding(
+                    padding: const EdgeInsets.all(5), // Border radius
+                    child: (ClipOval(child: Image.asset('image/close.png')))),
+                  ),
+              ],
             ),
-            color: Colors.green,
-            radius: 4.0,
-            onPressed: () {
+            onTap: () {
               Navigator.push(
                 context,
                 MaterialPageRoute(builder: (context) => Home()),
